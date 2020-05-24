83073e012e5f96bd4ab468575eadf80f78e63dca
Ollie Spurway <ollie.spurway@gmail.com>
2020-05-24 13:04:46 +0300

    set up the template linking using the static folder for resources

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/generalOverview')
+def overview():
+    return render_template('generalOverview_aboutGoldesp.html')
 
 
 if __name__ == "__main__":
